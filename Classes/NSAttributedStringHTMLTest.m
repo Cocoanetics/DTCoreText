eec0c67305d7eedc16ad4be1790acab90e94f9fc
Oliver Drobnik <oliver@drobnik.com>
2011-01-12 05:47:48 +0100

    Added paragraph unit test

--- Classes/NSAttributedStringHTMLTest.m
+++ Classes/NSAttributedStringHTMLTest.m
@@ -7,9 +7,39 @@
 //
 
 #import "NSAttributedStringHTMLTest.h"
-
+#import "NSAttributedString+HTML.h"
 
 @implementation NSAttributedStringHTMLTest
+
+
+- (void)testParagraphs
+{
+	NSString *html = @"Prefix<p>One\ntwo\n<br>three</p><p>New Paragraph</p>Suffix";
+	NSData *data = [html dataUsingEncoding:NSUTF8StringEncoding];
+	
+	NSAttributedString *string = [[NSAttributedString alloc] initWithHTML:data documentAttributes:NULL];
+	NSData *dump = [[string string] dataUsingEncoding:NSUTF8StringEncoding];
+	NSString *resultOnIOS = [dump description];
+	
+	NSString *resultOnMac = @"<50726566 69780a4f 6e652074 776f20e2 80a87468 7265650a 4e657720 50617261 67726170 680a5375 66666978>";
+	
+	STAssertEqualObjects(resultOnIOS, resultOnMac, @"Output on Paragraph Test differs");
+}
+
+
+- (void)testHeaderParagraphs
+{
+	NSString *html = @"Prefix<h1>One</h1><h2>One</h2><h3>One</h3><h4>One</h4><h5>One</h5><p>New Paragraph</p>Suffix";
+	NSData *data = [html dataUsingEncoding:NSUTF8StringEncoding];
+	
+	NSAttributedString *string = [[NSAttributedString alloc] initWithHTML:data documentAttributes:NULL];
+	NSData *dump = [[string string] dataUsingEncoding:NSUTF8StringEncoding];
+	NSString *resultOnIOS = [dump description];
+	
+	NSString *resultOnMac = @"<50726566 69780a4f 6e650a4f 6e650a4f 6e650a4f 6e650a4f 6e650a4e 65772050 61726167 72617068 0a537566 666978>";
+	
+	STAssertEqualObjects(resultOnIOS, resultOnMac, @"Output on Paragraph Test differs");
+}
 
 
 @end
