d48ca528c2a8bffe89a9dfd5ca922d87c31150ab
Sam Soffes <sam@samsoff.es>
2011-01-13 23:03:45 -0800

    Remove the need for Interface Builder

--- Classes/CoreTextExtensionsAppDelegate.h
+++ Classes/CoreTextExtensionsAppDelegate.h
@@ -6,17 +6,13 @@
 //  Copyright 2011 Drobnik.com. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
-
 @class CoreTextExtensionsViewController;
 
 @interface CoreTextExtensionsAppDelegate : NSObject <UIApplicationDelegate> {
+	
     UIWindow *window;
-    CoreTextExtensionsViewController *viewController;
 }
 
-@property (nonatomic, retain) IBOutlet UIWindow *window;
-@property (nonatomic, retain) IBOutlet CoreTextExtensionsViewController *viewController;
+@property (nonatomic, retain) UIWindow *window;
 
 @end
-
--- main.m
+++ main.m
@@ -6,12 +6,10 @@
 //  Copyright 2011 Drobnik.com. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
-
 int main(int argc, char *argv[]) {
     
     NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
-    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    int retVal = UIApplicationMain(argc, argv, nil, @"CoreTextExtensionsAppDelegate");
     [pool release];
     return retVal;
 }
