09a5dcc559cdffd6b70550cc63b4be1737a83ef6
Oliver Drobnik <oliver@drobnik.com>
2011-08-24 14:14:58 +0200

    Added missing header import to avoid warning

--- Classes/DTCoreTextFontDescriptor.m
+++ Classes/DTCoreTextFontDescriptor.m
@@ -18,6 +18,7 @@ static NSMutableDictionary *_fontOverrides = nil;
 #if __IPHONE_OS_VERSION_MIN_REQUIRED >= __IPHONE_4_3
 static dispatch_semaphore_t fontLock;
 #else
+#import <pthread.h>
 static pthread_mutex_t fontLock;
 #endif
 
