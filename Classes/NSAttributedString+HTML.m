a698638788679bb5c35f5451d7282fe7db980800
Oliver Drobnik <oliver@drobnik.com>
2011-08-25 17:42:24 +0200

    Correced wrong closing slash on img tag in htmlString method

--- Classes/NSAttributedString+HTML.m
+++ Classes/NSAttributedString+HTML.m
@@ -1133,7 +1133,7 @@ NSString *DTDefaultListIndent = @"DTDefaultListIndent";
 					}
 					
 					// end
-					[retString appendString:@" \\>"];
+					[retString appendString:@" />"];
 				}
 				
 				continue;
