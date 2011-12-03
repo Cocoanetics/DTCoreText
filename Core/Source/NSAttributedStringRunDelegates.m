aaca996dade7935ea2d655ea495ead46092e879e
David Hoerl <dhoerl@lot18.com>
2011-12-02 20:28:17 -0500

    Made the delegate file ARC compliant

--- Core/Source/NSAttributedStringRunDelegates.m
+++ Core/Source/NSAttributedStringRunDelegates.m
@@ -9,9 +9,6 @@
 #import "NSAttributedStringRunDelegates.h"
 #import "DTTextAttachment.h"
 
-#if __has_feature(objc_arc)
-#error "This file is NOT ARC compliant! Disable ARC with the -fno-objc-arc flag
-#endif
 
 void embeddedObjectDeallocCallback(void *context)
 {
@@ -19,16 +16,15 @@ void embeddedObjectDeallocCallback(void *context)
 
 CGFloat embeddedObjectGetAscentCallback(void *context)
 {
-	if ([(id)context isKindOfClass:[DTTextAttachment class]])
+	if ([(__bridge id)context isKindOfClass:[DTTextAttachment class]])
 	{
-		DTTextAttachment *attachment = context;
-		return [attachment displaySize].height;
+		return [(__bridge DTTextAttachment *)context displaySize].height;
 	}
 	return 0;
 }
 CGFloat embeddedObjectGetDescentCallback(void *context)
 {
-	if ([(id)context isKindOfClass:[DTTextAttachment class]])
+	if ([(__bridge id)context isKindOfClass:[DTTextAttachment class]])
 	{
 		return 0;
 	}
@@ -37,11 +33,10 @@ CGFloat embeddedObjectGetDescentCallback(void *context)
 
 CGFloat embeddedObjectGetWidthCallback(void * context)
 {
-	if ([(id)context isKindOfClass:[DTTextAttachment class]])
+	if ([(__bridge id)context isKindOfClass:[DTTextAttachment class]])
 	{
-		return [(DTTextAttachment *)context displaySize].width;
+		return [(__bridge DTTextAttachment *)context displaySize].width;
 	}
-	
 	return 35;
 }
 
@@ -53,6 +48,6 @@ CTRunDelegateRef createEmbeddedObjectRunDelegate(id obj)
 	callbacks.getAscent = embeddedObjectGetAscentCallback;
 	callbacks.getDescent = embeddedObjectGetDescentCallback;
 	callbacks.getWidth = embeddedObjectGetWidthCallback;
-	return CTRunDelegateCreate(&callbacks, (void *)obj);
+	return CTRunDelegateCreate(&callbacks, (__bridge void *)obj);
 	return NULL;
 }
