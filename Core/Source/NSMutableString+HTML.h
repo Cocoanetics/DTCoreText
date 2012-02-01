9079389044483c48c0defd062bd474ed281c9e4a
Oliver Drobnik <oliver@drobnik.com>
2012-02-01 09:13:07 +0100

    Added convenience function to trim whitespace at end of mutable string.

---
+++ Core/Source/NSMutableString+HTML.h
@@ -0,0 +1,15 @@
+//
+//  NSMutableString+HTML.h
+//  DTCoreText
+//
+//  Created by Oliver Drobnik on 01.02.12.
+//  Copyright (c) 2012 Drobnik.com. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSMutableString (HTML)
+
+- (void)removeWhitespaceSuffix;
+
+@end
