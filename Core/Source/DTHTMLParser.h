279880635349fa1b46f05721f0c39d07d869a86b
Oliver Drobnik <oliver@drobnik.com>
2012-02-06 15:40:03 +0100

    Fixed AppleDoc comments

--- Core/Source/DTHTMLParser.h
+++ Core/Source/DTHTMLParser.h
@@ -9,7 +9,8 @@
 #import <Foundation/Foundation.h>
 
 @class DTHTMLParser;
-
+/** The DTHTMLParserDelegate protocol defines the optional methods implemented by delegates of DTHTMLParser objects. 
+ */
 @protocol DTHTMLParserDelegate <NSObject>
 
 @optional
@@ -51,7 +52,7 @@
  The parser object may send the delegate several parser:foundCharacters: messages to report the characters of an element. Because string may be only part of the total character content for the current element, you should append it to the current accumulation of characters until the element changes.
  
  @param parser A parser object.
- @param A string representing the complete or partial textual content of the current element.
+ @param string A string representing the complete or partial textual content of the current element.
  */
 - (void)parser:(DTHTMLParser *)parser foundCharacters:(NSString *)string;
 
@@ -69,7 +70,7 @@
  Through this method the parser object passes the contents of the block to its delegate in an NSData object. The CDATA block is character data that is ignored by the parser. The encoding of the character data is UTF-8. To convert the data object to a string object, use the NSString method initWithData:encoding:. Note: CSS style blocks are returned as CDATA.
  
  @param parser A DTHTMLParser object parsing HTML.
- @param comment A data object containing a block of CDATA.
+ @param CDATABlock A data object containing a block of CDATA.
  */
 - (void)parser:(DTHTMLParser *)parser foundCDATA:(NSData *)CDATABlock;
 
@@ -85,7 +86,8 @@
 
 @end
 
-
+/** Instances of this class parse HTML documents (including DTD declarations) in an event-driven manner. A DTHTMLParser notifies its delegate about the items (elements, attributes, CDATA blocks, comments, and so on) that it encounters as it processes an HTML document. It does not itself do anything with those parsed items except report them. It also reports parsing errors. For convenience, an DTHTMLParser object in the following descriptions is sometimes referred to as a parser object.
+ */
 @interface DTHTMLParser : NSObject
 
 /**-------------------------------------------------------------------------------------
@@ -127,7 +129,7 @@
 /**
  Sets the receiver’s delegate.
  
- @param An object that is the new delegate. It is not retained. The delegate must conform to the `DTHTMLParserDelegate` Protocol protocol.
+ @param delegate An object that is the new delegate. It is not retained. The delegate must conform to the `DTHTMLParserDelegate` Protocol protocol.
  
  @see delegate
  */
