194320e4a8ca763de4cde604f0ceda5fd591e9d5
Oliver Drobnik <oliver@drobnik.com>
2012-05-28 09:13:51 +0200

    Added simple version struct

---
+++ Core/Source/UIDevice+DTSimpleVersion.h
@@ -0,0 +1,28 @@
+//
+//  UIDevice+DTSimpleVersion.h
+//  CoreTextExtensions
+//
+//  Created by Oliver Drobnik on 5/30/11.
+//  Copyright 2011 Drobnik.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+typedef struct
+{
+	NSInteger major;
+	NSInteger minor;
+	NSInteger point;
+} DTSimpleVersion;
+
+/** Convenience method to return the current OS version as a struct of three NSIntegers. Using UIDevice's `currentDevice` method and the current device's `systemVersion` returns a string delimited by a period which can then be split into an array. This method returns a struct storing each value instead of a string or array. Used in DTCoreTextLayoutFrame to workaround the way iOS 4.2 handles images. 
+ */
+@interface UIDevice (DTSimpleVersion)
+
+/**
+ Returns the current OS version of the device.
+ @returns A DTSimpleVersion struct with three fields each of type NSInteger storing the major, minor, and point numbers identifying this OS version. */
+- (DTSimpleVersion) osVersion;
+
+@end
---
+++ Core/Source/UIDevice+DTSimpleVersion.m
@@ -0,0 +1,32 @@
+//
+//  UIDevice+DTVersion.m
+//  CoreTextExtensions
+//
+//  Created by Oliver Drobnik on 5/30/11.
+//  Copyright 2011 Drobnik.com. All rights reserved.
+//
+
+#import "UIDevice+DTSimpleVersion.h"
+
+@implementation UIDevice (DTSimpleVersion)
+
+- (DTSimpleVersion) osVersion
+{
+	static DTSimpleVersion retVersion;
+	
+	static dispatch_once_t onceToken;
+	dispatch_once(&onceToken, ^{
+		NSString* versionString = [self systemVersion];
+		NSArray *parts = [versionString componentsSeparatedByString:@"."];
+		
+		NSUInteger partCount = [parts count];
+		
+		retVersion.major = (partCount>0)?[[parts objectAtIndex:0] intValue]:0;
+		retVersion.minor = (partCount>1)?[[parts objectAtIndex:1] intValue]:0;
+		retVersion.point = (partCount>2)?[[parts objectAtIndex:2] intValue]:0;
+	});
+	
+	return retVersion;
+}
+
+@end
