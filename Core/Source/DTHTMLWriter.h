0fe0de6e912a64d949abd3bc889a8c3485cc14b4
Oliver Drobnik <oliver@drobnik.com>
2012-12-23 10:14:48 +0100

    Moved HTML generation into DTHTMLWriter Added textScale property to reduce absolute px sizes if you generated the attributed string with a text size multiplicator. If set then this will divide all absolute font-size and line-height by this amount.

---
+++ Core/Source/DTHTMLWriter.h
@@ -0,0 +1,32 @@
+//
+//  DTHTMLWriter.h
+//  DTCoreText
+//
+//  Created by Oliver Drobnik on 23.12.12.
+//  Copyright (c) 2012 Drobnik.com. All rights reserved.
+//
+
+/**
+ Class to generate HTML from `NSAttributedString` instances.
+ */
+@interface DTHTMLWriter : NSObject
+
+/**
+ Creates a writer with a given `NSAttributedString` as input
+ @param attributedString An attributed string
+ */
+- (id)initWithAttributedString:(NSAttributedString *)attributedString;
+
+
+/**
+ Generates a HTML representation of the attributed string
+ @returns The generated string
+ */
+- (NSString *)HTMLString;
+
+/**
+ If specified then all absolute font sizes (px) will be divided by this value. This is useful if you specified a text size multiplicator when converting HTML to the attributed string you are processing.
+ */
+@property (nonatomic, assign) CGFloat textScale;
+
+@end
