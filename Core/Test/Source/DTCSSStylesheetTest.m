de7a4994fe8e65d203fe8c9eccd926a85e69facd
Oliver Drobnik <oliver@drobnik.com>
2013-01-27 19:16:09 +0100

    Added new unit test for merging stylesheet into one containing font shorthand

--- Core/Test/Source/DTCSSStylesheetTest.m
+++ Core/Test/Source/DTCSSStylesheetTest.m
@@ -60,4 +60,21 @@
 	STAssertEquals((float)element.fontDescriptor.pointSize, (float)40.0f, @"font size should be 40px");
 }
 
+// merge a stylesheet into one that has to decompress a font shorthand
+- (void)testMergingWithDecompression
+{
+	DTCSSStylesheet *stylesheet = [[DTCSSStylesheet alloc] initWithStyleBlock:@"p {font: italic small-caps bold 14.0px/100px \"Times New Roman\", serif;}"];
+	DTCSSStylesheet *otherStyleSheet = [[DTCSSStylesheet alloc] initWithStyleBlock:@"p {margin-bottom:30px;font-size:40px;}"];
+	[stylesheet mergeStylesheet:otherStyleSheet];
+	
+	NSDictionary *styles = [stylesheet.styles objectForKey:@"p"];
+	
+	STAssertEqualObjects(styles[@"font-size"], @"40px", @"Font Size should be 40px");
+	STAssertEqualObjects(styles[@"font-family"], @"\"Times New Roman\", serif", @"Font Family is wrong");
+	STAssertEqualObjects(styles[@"font-style"], @"italic", @"Font Style should be italic");
+	STAssertEqualObjects(styles[@"font-variant"], @"small-caps", @"Font Variant should be small-caps");
+	STAssertEqualObjects(styles[@"line-height"], @"100px", @"Line Height should be 100px");
+	STAssertEqualObjects(styles[@"margin-bottom"], @"30px", @"Margin Bottom should be 30px");
+}
+
 @end
