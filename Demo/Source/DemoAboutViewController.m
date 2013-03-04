bfc61498d1815acd707c40eb6d689310a3ae572c
Oliver Drobnik <oliver@cocoanetics.com>
2013-03-04 19:26:36 +0100

    Added About View that is loaded from XIB, as example for #326

---
+++ Demo/Source/DemoAboutViewController.m
@@ -0,0 +1,51 @@
+//
+//  DemoAboutViewController.m
+//  DTCoreText
+//
+//  Created by Oliver Drobnik on 3/4/13.
+//  Copyright (c) 2013 Drobnik.com. All rights reserved.
+//
+
+#import "DemoAboutViewController.h"
+
+@interface DemoAboutViewController ()
+
+@end
+
+@implementation DemoAboutViewController
+
+- (id)init
+{
+    self = [super initWithNibName:@"DemoAboutViewController" bundle:nil];
+    if (self)
+	 {
+        // Custom initialization
+		 self.navigationItem.title = @"About DTCoreText";
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+	
+	NSString *path = [[NSBundle mainBundle] pathForResource:@"About" ofType:@"html"];
+	NSData *data = [NSData dataWithContentsOfFile:path];
+	NSAttributedString *attributedString = [[NSAttributedString alloc] initWithHTMLData:data documentAttributes:NULL];
+	
+	self.attributedTextView.attributedString = attributedString;
+	self.attributedTextView.contentInset = UIEdgeInsetsMake(10, 10, 10, 10);
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (void)viewDidUnload {
+    [self setAttributedTextView:nil];
+    [super viewDidUnload];
+}
+@end
