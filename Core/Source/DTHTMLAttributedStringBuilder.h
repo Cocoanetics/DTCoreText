0137eca1d4f2b8727cff05dbf0219c2950686530
Oliver Drobnik <oliver@cocoanetics.com>
2013-03-11 15:11:34 +0100

    Moved documentation of parsing options to DTHTMLAttributedStringBuilder

--- Core/Source/DTHTMLAttributedStringBuilder.h
+++ Core/Source/DTHTMLAttributedStringBuilder.h
@@ -24,6 +24,25 @@ typedef void(^DTHTMLAttributedStringBuilderWillFlushCallback)(DTHTMLElement *);
 
 /**
  Initializes and returns a new `NSAttributedString` object from the HTML contained in the given object and base URL.
+ 
+ Options can be:
+ 
+ - DTMaxImageSize: the maximum CGSize that a text attachment can fill
+ - DTDefaultFontFamily: the default font family to use instead of Times New Roman
+ - DTDefaultFontSize: the default font size to use instead of 12
+ - DTDefaultTextColor: the default text color
+ - DTDefaultLinkColor: the default color for hyperlink text
+ - DTDefaultLinkDecoration: the default decoration for hyperlinks
+ - DTDefaultTextAlignment: the default text alignment for paragraphs
+ - DTDefaultLineHeightMultiplier: The multiplier for line heights
+ - DTDefaultFirstLineHeadIndent: The default indent for left margin on first line
+ - DTDefaultParagraphSpacing: The default spacing to apply after paragraphs
+ - DTDefaultHeadIndent: The default indent for left margin except first line
+ - DTDefaultListIndent: The amount by which lists are indented
+ - DTDefaultStyleSheet: The default style sheet to use
+ - DTUseiOS6Attributes: use iOS 6 attributes for building (UITextView compatible)
+ - DTWillFlushBlockCallBack: a block to be executed whenever content is flushed to the output string
+ 
  @param data The data in HTML format from which to create the attributed string.
  @param options Specifies how the document should be loaded. Contains values described in NSAttributedString(HTML).
  @param docAttributes Currently not in used.
--- Core/Source/NSAttributedString+HTML.h
+++ Core/Source/NSAttributedString+HTML.h
@@ -11,6 +11,7 @@
 /**
  Methods for generating an `NSAttributedString` from HTML data. Those methods exist on Mac but have not been ported (publicly) to iOS. This project aims to remedy this.
  
+ For a list of available options to pass to any of these methods please refer to [DTHTMLAttributedStringBuilder initWithHTML:options:documentAttributes:].
  */
 
 @interface NSAttributedString (HTML)
@@ -24,6 +25,7 @@
  @param data The data in HTML format from which to create the attributed string.
  @param docAttributes Currently not in used.
  @returns Returns an initialized object, or `nil` if the data can’t be decoded.
+ @see [DTHTMLAttributedStringBuilder initWithHTML:options:documentAttributes:] for a list of available options
  */
 - (id)initWithHTMLData:(NSData *)data documentAttributes:(NSDictionary **)docAttributes;
 
@@ -33,33 +35,18 @@
  @param baseURL An `NSURL` that represents the base URL for all links within the HTML.
  @param docAttributes Currently not in used.
  @returns Returns an initialized object, or `nil` if the data can’t be decoded.
+ @see [DTHTMLAttributedStringBuilder initWithHTML:options:documentAttributes:] for a list of available options
  */
 - (id)initWithHTMLData:(NSData *)data baseURL:(NSURL *)baseURL documentAttributes:(NSDictionary **)docAttributes;
 
 /**
- Initializes and returns a new `NSAttributedString` object from the HTML contained in the given object and base URL.
+ Initializes and returns a new `NSAttributedString` object from the HTML contained in the given object and base URL. 
  
- Options can be:
- 
- - DTMaxImageSize: the maximum CGSize that a text attachment can fill
- - DTDefaultFontFamily: the default font family to use instead of Times New Roman
- - DTDefaultFontSize: the default font size to use instead of 12
- - DTDefaultTextColor: the default text color
- - DTDefaultLinkColor: the default color for hyperlink text
- - DTDefaultLinkDecoration: the default decoration for hyperlinks
- - DTDefaultTextAlignment: the default text alignment for paragraphs
- - DTDefaultLineHeightMultiplier: The multiplier for line heights
- - DTDefaultFirstLineHeadIndent: The default indent for left margin on first line
- - DTDefaultHeadIndent: The default indent for left margin except first line
- - DTDefaultListIndent: The amount by which lists are indented
- - DTDefaultStyleSheet: The default style sheet to use
- - DTUseiOS6Attributes: use iOS 6 attributes for building (UITextView compatible)
- - DTWillFlushBlockCallBack: a block to be executed whenever content is flushed to the output string
-
  @param data The data in HTML format from which to create the attributed string.
  @param options Specifies how the document should be loaded.
  @param docAttributes Currently not in used.
  @returns Returns an initialized object, or `nil` if the data can’t be decoded.
+  @see [DTHTMLAttributedStringBuilder initWithHTML:options:documentAttributes:] for a list of available options
  */
 - (id)initWithHTMLData:(NSData *)data options:(NSDictionary *)options documentAttributes:(NSDictionary **)docAttributes;
 
