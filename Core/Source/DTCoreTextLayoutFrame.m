7d42a4a663e9af9ea7fe54a66db965d42474b5c3
Eric Böhnisch-Volkmann <eboehnisch@devontechnologies.com>
2013-04-26 11:50:26 +0200

    Added "dir=auto" to HTML output for elements with writing direction "natural"

--- Core/Source/DTCoreTextLayoutFrame.m
+++ Core/Source/DTCoreTextLayoutFrame.m
@@ -593,7 +593,8 @@ static BOOL _DTCoreTextLayoutFramesShouldDrawDebugFrames = NO;
 		lineOrigin.y = _frame.size.height - lineOrigin.y + _frame.origin.y;
 		lineOrigin.x += _frame.origin.x;
 		
-		DTCoreTextLayoutLine *newLine = [[DTCoreTextLayoutLine alloc] initWithLine:(__bridge CTLineRef)oneLine];		newLine.baselineOrigin = lineOrigin;
+		DTCoreTextLayoutLine *newLine = [[DTCoreTextLayoutLine alloc] initWithLine:(__bridge CTLineRef)oneLine];
+		newLine.baselineOrigin = lineOrigin;
 		
 		[tmpLines addObject:newLine];
 		
