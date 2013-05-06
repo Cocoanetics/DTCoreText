096e78c2df276c0fa4e2a6245ad3301fffe7b5bd
Oliver Drobnik <oliver@cocoanetics.com>
2013-05-06 12:32:08 +0200

    Added unit test for #435

--- Core/Test/Source/NSStringCSSTest.m
+++ Core/Test/Source/NSStringCSSTest.m
@@ -98,6 +98,18 @@
 	STAssertNil(shadows, @"Got back an array with %d entries instead of nil", [shadows count]);
 }
 
+- (void)testOneNoteStyle
+{
+	NSString *style = @"background-image:none;background-attachment:scroll;background-color:transparent;background-position-x:0%;background-position-y:0%;background-repeat:repeat;border-bottom-color:#000000;border-bottom-style:none;border-bottom-width:medium;border-left-color:#000000;border-left-style:none;border-left-width:medium;border-right-color:#000000;border-right-style:none;border-right-width:medium;border-top-color:#000000;border-top-style:none;border-top-width:medium;border-width:medium;clear:none;color:#000000;display:inline;font-family:Times New Roman;font-size:7pt;font-style:normal;font-variant:normal;letter-spacing:normal;line-height:normal;list-style-image:none;list-style-position:outside;list-style-type:disc;overflow:visible;padding:0px;padding-bottom:0px;padding-left:0px;padding-right:0px;padding-top:0px;position:static;float:none;text-align:left;text-decoration:none;text-indent:-0.25in;text-transform:none;visibility:inherit; FONT: 7pt &amp;quot;Times New Roman&amp;quot;";
+	
+	NSDictionary *styles = [style dictionaryOfCSSStyles];
+	
+	NSString *fontFamily = styles[@"font-family"];
+	STAssertTrue([fontFamily isEqualToString:@"Times New Roman"], @"Font Family should be Times");
+	
+	NSString *fontAttribute = styles[@"font"];
+	STAssertNil(fontAttribute, @"Uppercase FONT attribute shoudl be ignored");
+}
 
 
 @end
