181da3359826042c1bf3e2672ef938291e141af6
Lee Hericks <lee.hericks@me.com>
2013-05-21 10:25:53 +0900

    Added missing check for nil value sent to enumeration block.

--- Core/Source/NSAttributedString+DTCoreText.m
+++ Core/Source/NSAttributedString+DTCoreText.m
@@ -26,6 +26,12 @@
 	NSRange entireRange = NSMakeRange(0, [self length]);
 	[self enumerateAttribute:NSAttachmentAttributeName inRange:entireRange options:NSAttributedStringEnumerationLongestEffectiveRangeNotRequired usingBlock:^(DTTextAttachment *attachment, NSRange range, BOOL *stop) {
 		
+		if (attachment == nil)
+		{
+			// no attachment value
+			return;
+		}
+		
 		if (predicate && ![predicate evaluateWithObject:attachment])
 		{
 			// doesn't fit predicate, next
