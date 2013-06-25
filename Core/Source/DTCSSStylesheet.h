85f9b57ef92a91f14cd6e4e9d0725c7bd6364e36
Oliver Drobnik <oliver@cocoanetics.com>
2013-06-25 19:07:27 +0200

    added optioning out parameter to get matched selectors from merged style dictionary #491

--- Core/Source/DTCSSStylesheet.h
+++ Core/Source/DTCSSStylesheet.h
@@ -67,9 +67,10 @@
  Returns a dictionary that contains the merged style for a given element and the applicable style rules from the receiver.
  
  @param element The HTML element.
+ @param matchedSelectors The CSS selectors that caused a match
  @returns The merged style dictionary containing only styles which selector matches the element
  */
-- (NSDictionary *)mergedStyleDictionaryForElement:(DTHTMLElement *)element;
+- (NSDictionary *)mergedStyleDictionaryForElement:(DTHTMLElement *)element matchedSelectors:(NSSet **)matchedSelectors;
 
 
 /**
