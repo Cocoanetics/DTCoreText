Cocoanetics/DTCoreText

commit 81231510af5126b208cb59bf6291459f1930b4c7

Disable 4.3 leak fix for newer OS versions #535

Core/Source/NSMutableAttributedString+HTML.m
@@ -139,7 +139,12 @@
 				[mutableDictionary setObject:value forKey:name];
 				
 				// substitute attribute
-				[self removeAttribute:DTCustomAttributesAttribute range:effectiveRange];
+				if (NSFoundationVersionNumber <=  751.49)
+				{
+					// remove old (works around iOS 4.3 leak)
+					[self removeAttribute:DTCustomAttributesAttribute range:effectiveRange];
+				}
+
 				[self addAttribute:DTCustomAttributesAttribute value:[mutableDictionary copy] range:effectiveRange];
 			}
 			else
@@ -171,7 +176,11 @@
 			[mutableDictionary removeObjectForKey:name];
 			
 			// substitute attribute
-			[self removeAttribute:DTCustomAttributesAttribute range:effectiveRange];
+			if (NSFoundationVersionNumber <=  751.49)
+			{
+				// remove old (works around iOS 4.3 leak)
+				[self removeAttribute:DTCustomAttributesAttribute range:effectiveRange];
+			}
 			
 			// only re-add modified dictionary if it is not empty
 			if ([mutableDictionary count])

Core/Source/DTHTMLElement.m
@@ -489,8 +489,11 @@ NSDictionary *_classesForNames = nil;
 						// make new paragraph style
 						NSParagraphStyle *newParaStyle = [paragraphStyle NSParagraphStyle];
 						
-						// remove old (works around iOS 4.3 leak)
-						[tmpString removeAttribute:NSParagraphStyleAttributeName range:paragraphRange];
+						if (NSFoundationVersionNumber <=  751.49)
+						{
+							// remove old (works around iOS 4.3 leak)
+							[tmpString removeAttribute:NSParagraphStyleAttributeName range:paragraphRange];
+						}
 						
 						// set new
 						[tmpString addAttribute:NSParagraphStyleAttributeName value:newParaStyle range:paragraphRange];
@@ -510,8 +513,11 @@ NSDictionary *_classesForNames = nil;
 						// make new paragraph style
 						CTParagraphStyleRef newParaStyle = [paragraphStyle createCTParagraphStyle];
 						
-						// remove old (works around iOS 4.3 leak)
-						[tmpString removeAttribute:(id)kCTParagraphStyleAttributeName range:paragraphRange];
+						if (NSFoundationVersionNumber <=  751.49)
+						{
+							// remove old (works around iOS 4.3 leak)
+							[tmpString removeAttribute:(id)kCTParagraphStyleAttributeName range:paragraphRange];
+						}
 						
 						// set new
 						[tmpString addAttribute:(id)kCTParagraphStyleAttributeName value:(__bridge_transfer id)newParaStyle range:paragraphRange];

commit 1b48d711f0dcd498b2b37981c6c307779e305d04

Fixed error introduced in 81231510af5126b208cb59bf6291459f1930b4c7

Core/Source/NSMutableAttributedString+HTML.m
@@ -176,16 +176,21 @@
 			[mutableDictionary removeObjectForKey:name];
 			
 			// substitute attribute
-			if (NSFoundationVersionNumber <=  751.49)
-			{
-				// remove old (works around iOS 4.3 leak)
-				[self removeAttribute:DTCustomAttributesAttribute range:effectiveRange];
-			}
 			
 			// only re-add modified dictionary if it is not empty
 			if ([mutableDictionary count])
 			{
+				if (NSFoundationVersionNumber <=  751.49)
+				{
+					// remove old (works around iOS 4.3 leak)
+					[self removeAttribute:DTCustomAttributesAttribute range:effectiveRange];
+				}
+				
 				[self addAttribute:DTCustomAttributesAttribute value:[mutableDictionary copy] range:effectiveRange];
+			}
+			else
+			{
+				[self removeAttribute:DTCustomAttributesAttribute range:effectiveRange];
 			}
 		}
 	}];