97bcf592f5309ce9d07e5aa562cb190b58fece08
Oliver Drobnik <oliver@cocoanetics.com>
2013-09-09 11:17:22 +0200

    Fixed build warning, libtool does not like -ObjC

--- Travis-CI/script.sh
+++ Travis-CI/script.sh
@@ -2,4 +2,9 @@
 set -e
 
 xctool project DTCoreText.xcodeproj -scheme DemoApp build test -sdk iphonesimulator -arch i386
+
+echo "result $?"
+
 xctool project DTCoreText.xcodeproj -scheme MacUnitTest test
+
+echo "result $?"
