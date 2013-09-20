fc7edf0eda3f382f55e97a13433a768c3af63c2e
Oliver Drobnik <oliver@cocoanetics.com>
2013-09-20 10:41:28 +0200

    removed arch from xctool

--- Travis-CI/script.sh
+++ Travis-CI/script.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 
-xctool project DTCoreText.xcodeproj -scheme DemoApp build test -sdk iphonesimulator -arch i386
+xctool project DTCoreText.xcodeproj -scheme DemoApp build test -sdk iphonesimulator
 
 echo "result $?"
 
