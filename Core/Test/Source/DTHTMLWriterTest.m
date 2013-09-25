73df2fc15d16c4693ebba9c9dcf63b7bc4b9e320
Amro Mousa <amromousa@gmail.com>
2013-09-25 09:43:39 -0400

    test for nested lists with no text node in the item including the nested list

--- Core/Test/Source/DTHTMLWriterTest.m
+++ Core/Test/Source/DTHTMLWriterTest.m
@@ -54,4 +54,17 @@
 	STAssertTrue(rangeSpanUL.location == NSNotFound, @"Missing LI between span and UL");
 }
 
+- (void)testNestedListOutputWithoutTextNode
+{
+	NSAttributedString *attributedString = [self attributedStringFromHTMLString:@"<ul><li><ol><li>2a</li><li>2b</li></ol></li><li>1a</li></ul>" options:NULL];
+//	NSAttributedString *attributedString = [self attributedStringFromHTMLString:@"<ol><li><ul><li>1a</li></ul></li><li>1b</li></ol>" options:NULL];
+	
+	// generate html
+	DTHTMLWriter *writer = [[DTHTMLWriter alloc] initWithAttributedString:attributedString];
+	NSString* html = [[writer HTMLFragment] stringByReplacingOccurrencesOfString:@"\n" withString:@""];
+	
+	NSRange twoAOutsideOL = [html rangeOfString:@"2a</span><ol"];
+	STAssertTrue(twoAOutsideOL.location != NSNotFound, @"List item 2a should not be outside the ordered list");
+}
+
 @end
