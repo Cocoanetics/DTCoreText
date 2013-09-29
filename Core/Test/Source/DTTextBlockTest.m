49c845a97a201386fbbc188fa002f612ab77ebb0
Oliver Drobnik <oliver@cocoanetics.com>
2013-09-29 21:17:42 +0200

    Added more DTTextBlock tests

--- Core/Test/Source/DTTextBlockTest.m
+++ Core/Test/Source/DTTextBlockTest.m
@@ -21,6 +21,19 @@
 	block2.padding = DTEdgeInsetsMake(10, 20, 30, 40);
 
 	STAssertEqualObjects(block1, block2, @"Both blocks should be equal");
+	
+	STAssertFalse([block1 isEqual:nil], @"isEqual:nil should be false");
+	STAssertFalse([block1 isEqual:@"bla"], @"isEqual: to string should be false");
+}
+
+- (void)testHash
+{
+	DTTextBlock *block1 = [[DTTextBlock alloc] init];
+	block1.padding = DTEdgeInsetsMake(10, 20, 30, 40);
+	
+	NSUInteger hash = [block1 hash];
+	
+	STAssertEquals(hash, 201010757, @"hash should be 201010757");
 }
 
 @end
