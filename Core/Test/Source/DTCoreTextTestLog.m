8aa420924747909d1dbfa93ff0f7310ead44df3d
Oliver Drobnik <oliver@cocoanetics.com>
2013-09-29 14:52:27 +0200

    Trying to fix

--- Core/Test/Source/DTCoreTextTestLog.m
+++ Core/Test/Source/DTCoreTextTestLog.m
@@ -11,27 +11,40 @@
 // GCOV Flush function
 extern void __gcov_flush(void);
 
+static id mainSuite = nil;
+
 @implementation DTCoreTextTestLog
 
 + (void)initialize
 {
-	[[NSUserDefaults standardUserDefaults] setValue:@"DTCoreTextTestLog"
-														  forKey:SenTestObserverClassKey];
+	[[NSUserDefaults standardUserDefaults] setValue:@"DTCoreTextTestLog" forKey:SenTestObserverClassKey];
 	
 	[super initialize];
 }
 
-+ (void)testSuiteDidStart:(NSNotification *)aNotification
++ (void)testSuiteDidStart:(NSNotification *)notification
 {
-	[super testSuiteDidStart:aNotification];
+	[super testSuiteDidStart:notification];
+	
+	SenTestSuiteRun *suite = notification.object;
+	
+	if (mainSuite == nil)
+	{
+		mainSuite = suite;
+	}
 }
 
 + (void)testSuiteDidStop:(NSNotification *)notification
 {
 	[super testSuiteDidStop:notification];
 	
-	// workaround for missing flush with iOS 7 Simulator
-	__gcov_flush();
+	SenTestSuiteRun* suite = notification.object;
+	
+	if (mainSuite == suite)
+	{
+		// workaround for missing flush with iOS 7 Simulator
+		__gcov_flush();
+	}
 }
 
 @end
