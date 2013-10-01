a844dfafdb607ad908387083f6d9726cee1412ce
Oliver Drobnik <oliver@cocoanetics.com>
2013-10-01 12:01:30 +0200

    improved coveralls script

--- coveralls.rb
+++ coveralls.rb
@@ -3,28 +3,73 @@
 require 'etc'
 require 'fileutils'
 require 'find'
+require 'optparse'
+
+excludedFolders = []
+
+OptionParser.new do |opts|
+  opts.banner = "Usage: example.rb [options]"
+
+  opts.on('-e', '--exclude FOLDER', 'Folder to exclude') do |v|
+     excludedFolders << v
+  end
+
+end.parse!(ARGV)
 
 workingDir = Dir.getwd
 derivedDataDir = "#{Etc.getpwuid.dir}/Library/Developer/Xcode/DerivedData/"
- 
+
 outputDir = workingDir + "/gcov"
+#FileUtils.rm_r outputDir
 FileUtils.mkdir outputDir 
- 
+
+GCOV_SOURCE_PATTERN = Regexp.new(/Source:(.*)/)
+
 Find.find(derivedDataDir) do |file|
   if file.match(/\.gcda\Z/)
-     
       #get just the folder name
       gcov_dir = File.dirname(file)
-      basename = File.basename(file)
-     
-      #chdir because gcov cannot work with absolute path
-      Dir.chdir gcov_dir
+
+      puts "\nINPUT: #{file}"
 
       #process the file
-      system("gcov '#{basename}' -o '#{gcov_dir}'")
+#      system "gcov", file, "-o", gcov_dir
       
-      Dir.glob("*.gcov") do |file|
-        FileUtils.mv(file, outputDir)
+      result = %x( gcov '#{file}' -o '#{gcov_dir}' )
+      
+      if (!result)
+        break
+      end
+      
+      # filter the resulting output
+      Dir.glob("*.gcov") do |gcov_file|
+        
+        firstLine = File.open(gcov_file).readline
+        match = GCOV_SOURCE_PATTERN.match(firstLine)
+        
+        if (match)
+          source_path = match[1]
+
+          if (source_path.start_with? workingDir)
+            
+            # cut off absolute working dir to get relative source path
+            relative_path = source_path.slice(workingDir.length+1, source_path.length)
+            
+            # get the path components
+            path_comps = relative_path.split(File::SEPARATOR)
+            
+            if (excludedFolders.include?(path_comps[0]))
+              puts "   - ignore #{relative_path} (excluded via option)"
+              FileUtils.rm gcov_file
+            else
+              puts "   - process: #{relative_path}"
+              FileUtils.mv(gcov_file, outputDir)
+            end
+          else
+            puts "   - ignore: #{gcov_file} (outside source folder)"
+            FileUtils.rm gcov_file
+          end
+        end
       end
    end
 end
