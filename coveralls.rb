3098628eeb4ff7870f75c1286f40e70b4123d5ac
Oliver Drobnik <oliver@cocoanetics.com>
2013-10-01 13:08:47 +0200

    improved coveralls script

--- coveralls.rb
+++ coveralls.rb
@@ -5,41 +5,56 @@ require 'fileutils'
 require 'find'
 require 'optparse'
 
+# arraw of source subfolders to exclude
 excludedFolders = []
 
-OptionParser.new do |opts|
-  opts.banner = "Usage: example.rb [options]"
+# create option parser
+opts = OptionParser.new
+opts.banner = "Usage: coveralls.rb [options]"
 
-  opts.on('-e', '--exclude FOLDER', 'Folder to exclude') do |v|
-     excludedFolders << v
-  end
+opts.on('-e', '--exclude FOLDER', 'Folder to exclude') do |v|
+   excludedFolders << v
+end
+  
+opts.on_tail("-h", "--help", "Show this message") do
+  puts opts
+  exit
+end
+  
+# parse the options
+begin      
+  opts.parse!(ARGV)
+rescue OptionParser::InvalidOption => e
+  puts e
+  puts opts
+  exit(1)
+end
 
-end.parse!(ARGV)
-
+# the folders
 workingDir = Dir.getwd
 derivedDataDir = "#{Etc.getpwuid.dir}/Library/Developer/Xcode/DerivedData/"
-
 outputDir = workingDir + "/gcov"
-#FileUtils.rm_r outputDir
+
+# create gcov output folder
 FileUtils.mkdir outputDir 
 
+# pattern to get source file from first line of gcov file
 GCOV_SOURCE_PATTERN = Regexp.new(/Source:(.*)/)
 
-Find.find(derivedDataDir) do |file|
-  if file.match(/\.gcda\Z/)
-      #get just the folder name
-      gcov_dir = File.dirname(file)
+# enumerate all gcda files underneath derivedData
+Find.find(derivedDataDir) do |gcda_file|
 
-      puts "\nINPUT: #{file}"
+  if gcda_file.match(/\.gcda\Z/)
+    
+      #get just the folder name
+      gcov_dir = File.dirname(gcda_file)
+
+      # cut off absolute working dir to get relative source path
+      relative_input_path = gcda_file.slice(derivedDataDir.length, gcda_file.length)
+      puts "\nINPUT: #{relative_input_path}"
 
       #process the file
-#      system "gcov", file, "-o", gcov_dir
-      
-      result = %x( gcov '#{file}' -o '#{gcov_dir}' )
-      
-      if (!result)
-        break
-      end
+      result = %x( gcov '#{gcda_file}' -o '#{gcov_dir}' )
       
       # filter the resulting output
       Dir.glob("*.gcov") do |gcov_file|
@@ -51,7 +66,6 @@ Find.find(derivedDataDir) do |file|
           source_path = match[1]
 
           if (source_path.start_with? workingDir)
-            
             # cut off absolute working dir to get relative source path
             relative_path = source_path.slice(workingDir.length+1, source_path.length)
             
@@ -59,14 +73,14 @@ Find.find(derivedDataDir) do |file|
             path_comps = relative_path.split(File::SEPARATOR)
             
             if (excludedFolders.include?(path_comps[0]))
-              puts "   - ignore #{relative_path} (excluded via option)"
+              puts "   - ignore:  #{relative_path} (excluded via option)"
               FileUtils.rm gcov_file
             else
               puts "   - process: #{relative_path}"
               FileUtils.mv(gcov_file, outputDir)
             end
           else
-            puts "   - ignore: #{gcov_file} (outside source folder)"
+            puts "   - ignore:  #{gcov_file} (outside source folder)"
             FileUtils.rm gcov_file
           end
         end
@@ -74,11 +88,8 @@ Find.find(derivedDataDir) do |file|
    end
 end
 
-#change back to working directory
-Dir.chdir workingDir
-
 #call the coveralls, exclude some files
-system 'coveralls', '-e', 'Externals', '-e', 'Test', '-e', "Demo"
+system 'coveralls'
 
 #clean up
 FileUtils.rm_rf outputDir
