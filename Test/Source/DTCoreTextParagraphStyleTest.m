ba4359fd628af6ef9e2261d62715bc0fbc69487a
Oliver Drobnik <oliver@cocoanetics.com>
2013-10-02 15:40:06 +0200

    fixed wrong comparison

--- Test/Source/DTCoreTextParagraphStyleTest.m
+++ Test/Source/DTCoreTextParagraphStyleTest.m
@@ -53,7 +53,7 @@
 	#if TARGET_OS_IPHONE
 	
 	// this test doesn't work running before iOS 7
-	if (NSFoundationVersionNumber <= DTNSFoundationVersionNumber_iOS_7_0)
+	if (NSFoundationVersionNumber < DTNSFoundationVersionNumber_iOS_7_0)
 	{
 		return;
 	}
