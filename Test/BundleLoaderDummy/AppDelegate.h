e4b3eae27ed030cb7f4f6f913685f5cff59830af
Oliver Drobnik <oliver@cocoanetics.com>
2013-10-05 12:21:38 +0200

    Added DummyBundleLoader

---
+++ Test/BundleLoaderDummy/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  UnitTestBundleLoader
+//
+//  Created by Oliver Drobnik on 10/5/13.
+//  Copyright (c) 2013 Drobnik.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Test/BundleLoaderDummy/AppDelegate.m
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.m
+//  UnitTestBundleLoader
+//
+//  Created by Oliver Drobnik on 10/5/13.
+//  Copyright (c) 2013 Drobnik.com. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    [self.window makeKeyAndVisible];
+    return YES;
+}
+
+@end
