2667e486e77ef2b00ea5de4e04c9154f4f75a464
李响 <applelixiang@126.com>
2016-10-12 10:23:24 +0800

    -[__NSCFConstantString substringToIndex:]: Index 2 out of bounds; string length 0

--- Core/Source/DTIframeTextAttachment.m
+++ Core/Source/DTIframeTextAttachment.m
@@ -24,8 +24,10 @@
 		NSString *src = [element.attributes objectForKey:@"src"];
 		
 		// prepend http: if URL string starts with // (seems to do with youtube iframes as standard)
-		if ([[src substringToIndex:2] isEqualToString:@"//"]) {
-			src = [@"http:" stringByAppendingString:src];
+		if (src.length){
+			if ([[src substringToIndex:2] isEqualToString:@"//"]) {
+				src = [@"http:" stringByAppendingString:src];
+			}
 		}
 		
 		// content URL
