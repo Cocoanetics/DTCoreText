b55cb01eec51ded2fb5c59d6eeedddef24d567e9
lisi <lisi_1987@foxmail.com>
2017-03-01 14:53:27 +0800

    support <p> text-indent

--- Core/Source/DTHTMLElement.h
+++ Core/Source/DTHTMLElement.h
@@ -76,6 +76,9 @@
 	// indent of lists
 	CGFloat _listIndent;
 	
+	// indent of tag <p>
+	CGFloat _pTextIndent;
+	
 	BOOL _shouldProcessCustomHTMLAttributes;
 }
 
@@ -168,6 +171,11 @@
  Background stroke width in the receiver
  */
 @property (nonatomic, assign) CGFloat backgroundCornerRadius;
+
+/**
+ Tag <p> text indent
+ */
+@property (nonatomic, assign) CGFloat pTextIndent;
 
 /**
  The custom letter spacing of the receiver, default is 0px
