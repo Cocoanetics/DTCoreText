6657645b9c1e1faa201a63f2370f3c34c9ee3881
Oliver Drobnik <oliver@cocoanetics.com>
2020-09-17 10:46:19 +0200

    Fixed missing animated GIF

--- Package.swift
+++ Package.swift
@@ -4,29 +4,32 @@
 import PackageDescription
 
 let package = Package(
-    name: "DTCoreText",
+    name: "DTFoundation",
     platforms: [
         .iOS(.v9),         //.v8 - .v13
         .macOS(.v10_10),    //.v10_10 - .v10_15
         .tvOS(.v9),        //.v9 - .v13
     ],
     products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "DTCoreText",
-            targets: ["DTCoreText"]),
-    ],
-    dependencies: [
-        .package(url: "https://github.com/Cocoanetics/DTFoundation.git", from: "1.7.15"),
+            name: "DTFoundation",
+            targets: ["DTFoundation"]),
     ],
     targets: [
         .target(
-            name: "DTCoreText",
-            dependencies: [
-                .product(name: "DTFoundation", package: "DTFoundation"),
-            ],
-            path: "Core"),
+            name: "DTFoundation",
+            dependencies: [],
+            path: "Core",
+            cSettings: [
+                .headerSearchPath("include/DTFoundation"),
+                .headerSearchPath("Source/Externals/minizip"),
+                .define("TARGET_OS_IPHONE=1", .when(platforms: [.iOS, .tvOS])),
+                .define("TARGET_OS_OSX=1", .when(platforms: [.macOS])),
+            ]
+        ),
         .testTarget(
-            name: "DTCoreTextTests",
-            dependencies: ["DTCoreText"]),
+            name: "DTFoundationTests",
+            dependencies: ["DTFoundation"]),
     ]
 )
