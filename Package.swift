e7d39111ea86e2769287c920ccd76b974cbfd125
Oliver Drobnik <oliver@cocoanetics.com>
2020-09-17 10:49:06 +0200

    Revert "Fixed missing animated GIF"
    This reverts commit 6657645b9c1e1faa201a63f2370f3c34c9ee3881.

--- Package.swift
+++ Package.swift
@@ -4,32 +4,29 @@
 import PackageDescription
 
 let package = Package(
-    name: "DTFoundation",
+    name: "DTCoreText",
     platforms: [
         .iOS(.v9),         //.v8 - .v13
         .macOS(.v10_10),    //.v10_10 - .v10_15
         .tvOS(.v9),        //.v9 - .v13
     ],
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "DTFoundation",
-            targets: ["DTFoundation"]),
+            name: "DTCoreText",
+            targets: ["DTCoreText"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/Cocoanetics/DTFoundation.git", from: "1.7.15"),
     ],
     targets: [
         .target(
-            name: "DTFoundation",
-            dependencies: [],
-            path: "Core",
-            cSettings: [
-                .headerSearchPath("include/DTFoundation"),
-                .headerSearchPath("Source/Externals/minizip"),
-                .define("TARGET_OS_IPHONE=1", .when(platforms: [.iOS, .tvOS])),
-                .define("TARGET_OS_OSX=1", .when(platforms: [.macOS])),
-            ]
-        ),
+            name: "DTCoreText",
+            dependencies: [
+                .product(name: "DTFoundation", package: "DTFoundation"),
+            ],
+            path: "Core"),
         .testTarget(
-            name: "DTFoundationTests",
-            dependencies: ["DTFoundation"]),
+            name: "DTCoreTextTests",
+            dependencies: ["DTCoreText"]),
     ]
 )
